94b5563d2f634b527698e32f98b58135c92cd9ee
nmeee02 <fil.levis@mail.ru>
2024-01-23 12:02:14 +0300

    Добавьте файлы проекта.

---
+++ variant1 aquatoria/oknoclient.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace variant1_aquatoria
+{
+    /// <summary>
+    /// Логика взаимодействия для oknoclient.xaml
+    /// </summary>
+    public partial class oknoclient : Window
+    {
+        public oknoclient()
+        {
+            InitializeComponent();
+        }
+    }
+}
